4d9a9efa786e1294b0f047b428094fdcde6f6c4f
ElanaBanana <s191591@students.aie.edu.au>
2024-02-16 17:03:55 +1030

    AT3, mostly implemented
    file access is working, and writing to file, just need to implement all the function tests

--- AssessmentTasks/AT1/main.cpp
+++ AssessmentTasks/AT1/main.cpp
@@ -1,6 +1,10 @@
 #include "String.h"
 #include "InputValidation.h"
 #include <iostream>
+#include <fstream> //files!!
+#include <ctime> // for time
+#include <cmath> // for ceil !! (to round)
+#include <iomanip> // for set precision
 using namespace std;
 
 /*
@@ -9,11 +13,12 @@ Elana Parnis
 */
 
 int TakeInput();
+void testFunctions(int* passArray);
 
 int main() {
 	cout << "Hi!" << endl;
 	cout << "Welcome to Elana's AT1 and AT2!" << endl;
-	cout << "Are you here to test for AT1 (1) or AT2 (2)?: " << endl;
+	cout << "Are you here to test for AT1 (1), AT2 (2) or AT3 (3)?: " << endl;
 	int input = TakeInput();
 	if (input == 1) 
 	{
@@ -40,7 +45,7 @@ int main() {
 		cout << "Welcome to my humble abode...\n" << endl;
 		cout << julianInput.CStr() << endl;
 	}
-	else 
+	else if (input == 2)
 	{
 		char array[8] = { 'H','e','l','l','o',',',' ','\0' };
 		//char array2[7] = { 'W','o','r','l','d','!','\0'};
@@ -232,6 +237,92 @@ int main() {
 		newString += str1;
 		cout << "newString + str1 is: " << newString.CStr() << endl;
 	}
+	else { //AT3
+		//run all the tests
+		//save outcome of test to an array
+		//write to file if the test passed or failed, 1== pass, 0== fail
+
+		const int numOfTests = 21;
+		//an array of strings which stores the name of all the functions being tested
+		String allTestNames[] = { 
+			String("Length"), 
+			String("CharacterAt"), 
+			String("EqualTo"),
+			String("Append"),
+			String("Prepend"),
+			String("CStr"),
+			String("ToLower"),
+			String("ToUpper"),
+			String("Find"),
+			String("FindAtIndex"),
+			String("Replace"),
+			String("ReadFromConsole"),
+			String("WriteToConsole"),
+			String("Operator=="),
+			String("Operator!="),
+			String("Operator="),
+			String("Operator<"),
+			String("Operator>"),
+			String("Operator[]"),
+			String("Operator+"),
+			String("Operator+=")
+		};
+
+		int testPassed[numOfTests] = { 0 };
+		//test all functions in String class
+		testFunctions(&testPassed[0]);
+
+		//check how many tests passed
+		float success = 0; //how many tests passed
+		for (int i = 0; i < numOfTests; i++) {
+			if (testPassed[i] == 1) {
+				success++;
+			}
+		}
+		//tests are ran in order they are listed in the Sting.h file
+
+					//FILE STUFF
+		//lets create and open a new file
+		ofstream outputFile; 
+		//open file for appending, create if doesnt exisit
+		outputFile.open("testOutput.txt", ios::out | ios::app);
+
+		//the date/ time based on the current system
+		time_t timeNow = time(0);
+		tm buff;
+		//gets the time from timeNow and stores it into the buff struct
+		if (localtime_s(&buff, &timeNow) != 0) {
+			//if failed to get the time
+			cout << "Failed to get the local time" << endl;
+		}
+
+		//using string to store the Date and Time to make output neater
+		//string Date = to_string(buff.tm_mday) + "/" + to_string(buff.tm_mon + 1) + "/" + to_string(buff.tm_year + 1900);
+		//string Time = to_string(buff.tm_hour) + ":" + to_string(30 + buff.tm_min) + ":" + to_string(buff.tm_sec);
+		//outputFile << "Date: " << Date << " Time: " << Time << " Success Rate: " << endl;
+
+		//without using strings
+		outputFile << "Date: " << buff.tm_mday << "/" << buff.tm_mon + 1 << "/" << buff.tm_year + 1900;
+		outputFile << " Time: " << buff.tm_hour << ":" << 30 + buff.tm_min << ":" << buff.tm_sec;
+		outputFile << " Success Rate: " << setprecision(3) << (success/numOfTests)*100.00 << "%" << endl;
+
+		//write out the success of all tests
+		for (int i = 0; i < numOfTests; i++) {
+			outputFile << "Test " << i + 1 << " " << allTestNames[i].CStr() << " ";
+			if (testPassed[i] == 1) 
+			{ //if the test passed
+				outputFile << "Successful" << endl;
+			}
+			else
+			{
+				outputFile << "Failed" << endl;
+			}
+		}
+
+		//close the file once complete
+		outputFile.close(); 
+	}
+
 	system("pause");
 	return 0;
 }
@@ -242,10 +333,48 @@ int TakeInput() { //function to make sure the input is only a 1 or 2 (intInput, 
 	while (correctInput == false) 
 	{
 		inputNum = intInput();
-		if (inputNum == 1 || inputNum == 2)
+		if (inputNum == 1 || inputNum == 2 || inputNum == 3)
 		{
 			return inputNum;
 		}
 		cout << "It needs to be a 1 or 2 !" << endl;
 	}
+}
+
+void testFunctions(int* passArray) {
+	//test all function
+	String test1("King Kong"); //len 9
+	String test2("Awesome Sauce! "); //len 14
+	String test3("Julien"); // len 6
+				//Length - test 0
+	//len of test1 = 9
+	int lenOfTest1 = 9;
+	if (lenOfTest1 == test1.Length()) {
+		passArray[0] = 1;
+	}
+				//characterAt - test 1
+	//Character at index 13 of test2 = !
+	char charAt13 = '!';
+	if (charAt13 == test2.CharacterAt(13)) {
+		passArray[1] = 1;
+	}
+				//equalTo - test 2
+				//append - test 3	
+				//prepend - test 4
+				//CStr - test 5
+				//ToLower - test 6	
+				//ToUpper - test 7	
+				//Find - test 8
+				//FindAtIndex - test 9	
+				//Replace - test 10
+				//ReadFromConsole - test 11
+				//WriteToConsole - test 12
+				//operator== - test 13
+				//operator!= - test 14
+				//operator= - test 15	
+				//operator< - test 16
+				//operator> - test 17
+				//operator[] - test 18	
+				//operator+ - test 19
+				//operator+= - test 20
 }
